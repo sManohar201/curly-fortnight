552143324328e1c785c5292bafe39d205d6bd3f9
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-03-21 00:34:25 -0400

    Updating script and launch files for track script

--- catkin_ws/src/obj_track/scripts/joystick_controlstart.py
+++ catkin_ws/src/obj_track/scripts/joystick_controlstart.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-# joy_trigger_start.py
+# joystick_controlstart.py
 # Use joystick input to launch exploration nodes in jackal
 # Intro to Robotics - EE5900 - Spring 2017
-#          Assignment #5
+#          Assignment #6
 
 #       Project #6 Group #4
 #            Haden
@@ -42,17 +42,14 @@ class joy_control(object):
         self.x = 0
         self.circ = 0
         self.sq = self.tri = self.L1 = self.R1 = self.share = self.options = self.p4 = self.L3 = self.R3 = self.DL = self.DR = self.DU = self.DD = 0
-        #self.llr, self.lud, self.L2, self.rlr, self.rud, self.R2 = 0
 
         self.active = False
-        #self.trigger = False
-        #self.explore_mode = 0
         objtrack_process = None
-        #timer_start = 0
+
         rospy.loginfo("In start")
 
         while not rospy.is_shutdown():
-            #if (self.trigger == True):
+
 
             if self.active == False:
 
@@ -60,9 +57,9 @@ class joy_control(object):
                 if (self.circ == 1):
                     rospy.loginfo("Joystick code received, commencing object tracking protocol...")
                     self.active = True
-                    # run = wall_avoid.WallAvoid(runtime)
+
                     package = 'obj_track'
-                    executable = 'objtrack.py' # NEED TO UPDATE ACTUAL SCRIPT NAME
+                    executable = 'track.py' # NEED TO UPDATE ACTUAL SCRIPT NAME
 
                     node = roslaunch.core.Node(package, executable)
                     launch = roslaunch.scriptapi.ROSLaunch()
@@ -75,7 +72,7 @@ class joy_control(object):
                     rospy.loginfo("Joystick code recieved, terminating object tracking protocol")
                     self.active = False
                     objtrack_process.stop()
-            #self.trigger = False
+
             self.x = 0
             self.circ = 0
             #rate.sleep()
@@ -83,19 +80,6 @@ class joy_control(object):
     def joy_callback(self, data):
 
         self.x, self.circ, self.sq, self.tri, self.L1, self.R1, self.share, self.options, self.p4, self.L3, self.R3, self.DL, self.DR, self.DU, self.DD = data.buttons
-        #llr, lud, L2, rlr, rud, R2 = data.axes
-
-        # # Start Object Tracking
-        # if (x == 1) and (L2 < 0.9) and (R2 < 0.9) and (self.ready == False):
-        #     rospy.loginfo("Controller code received, commencing exploration protocol...")
-        #     self.trigger = True
-        #     self.ready = True
-        # # Stop Object Tracking
-        # if (tri == 1) and (L2 < 0.9) and (R2 < 0.9) and (self.ready == True):
-        #     rospy.loginfo("Controller code received, terminating exploration protocol...")
-        #     self.trigger = True
-        #     self.ready = False
-        # rospy.sleep(1)
 
 
 if __name__ == "__main__":
