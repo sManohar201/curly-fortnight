c852425b390eb6dd11b3dffdc9ea6bba7300deda
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-03-20 22:38:33 -0400

    Updating launch and script files forlaunching remote nodes

--- catkin_ws/remote-jackal.sh
+++ catkin_ws/remote-jackal.sh
@@ -2,11 +2,12 @@
 
 if [ "$#" -eq 1 ]; then
   export ROS_MASTER_URI=http://$1:11311   # Hostname for Jackal
-  export ROS_IP="$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+  export ROS_IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 else
   export ROS_MASTER_URI=http://localhost:11311
   unset ROS_IP
 fi
 
 package_path="$(echo $ROS_PACKAGE_PATH | cut -d":" -f1)"
-#export JACKAL_URDF_EXTRAS="$package_path/building_mapper/urdf/sick_lm2xx.urdf.xacro"
+# export JACKAL_URDF_EXTRAS="$package_path/obj_track/SICK.urdf.xacro"
+export ROSLAUNCH_SSH_UNKNOWN=1
